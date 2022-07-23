51f8c90a2694b7afa7cf2295970f44902575f480
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-23 13:58:24 +0500

    sef3`

--- bin/map_basic03.dart
+++ bin/map_basic03.dart
@@ -8,7 +8,7 @@ Return true if the length of the map is greater than zero, otherwise false.
 
 bool func(Map dct) {
   Map data = {1: 'ona', 2: 'ota', 3: 'aka'};
-  if (data.length > 0) {
+  if (data.length < 0) {
     return true;
   } else
     return false;
